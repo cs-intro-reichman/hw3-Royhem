a20a7a5f4606d8eff27ebce2b0aedd30362bcbf9
Roy hemelniker <roy.hemelniker@post.runi.ac.il>
2025-11-20 11:18:18 +0200

    Completed assignment

--- Algebra.java
+++ Algebra.java
@@ -26,20 +26,40 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 
+		if(x2>0)
+		{
 		for(int i=0;i<x2;i++)
 		{
 			x1++;
+		}	
 		}
+		else
+		{
+			for(int i=0;i>x2;i++)
+		{
+			x1--;
+		}
+		}
+		
 		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 
-
-		for(int i=0;i<x2;i++)
+		if(x2>0)
+		{	
+for(int i=0;i<x2;i++)
 		{
 			x1--;
+		}
+		}
+		else
+		{
+		for(int i=0;i>x2;i++)
+		{
+			x1++;
+		}
 		}
 		return x1;		
 	}
@@ -48,37 +68,68 @@ public class Algebra {
 	public static int times(int x1, int x2) {
 
 		int sum=0;
-		for(int i=0;i<x2;i++)
+
+		if(x1==0||x2==0)
+			return 0;
+
+		boolean isNegative = false;
+        if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0))
+			isNegative= true;
+
+		if (x1<0)
 		{
-			sum=plus(x1, sum);
+			x1=minus(0, x1);
 		}
-		return sum;			
+
+		if (x2<0)
+		{
+			x2=minus(0, x2);
+		}
+		
+
+
+		for(int i = 0; i < x2; i ++) { 
+            sum = plus(sum, x1);
+		}
+
+		if (isNegative) {
+			return minus(0, sum);
+		}
+		return sum;
+
+
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		int sum=0;
+
+		int sum=x;
 			if(n==0)
 			{
 				return 1;
 			}
 			else if (n>0) 
 			{
+			
+
 				for(int i=1;i<n;i++)
 				{
-				x=times(x, x);
+				sum=times(sum, x);
 				}
 				
 			}
-			
 
-
-		return x;
+		return sum;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		int sum=0;
+
+		if (x2==0||x1==0)
+		{
+			return 0;
+		}
 		while (x1>=x2)
 		{
 
@@ -91,7 +142,8 @@ public class Algebra {
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-
+			if (x2 == 0) return x1;
+			if (x1==0) return 0;
 		
 		while (x1>=x2)
 		{
@@ -103,6 +155,8 @@ public class Algebra {
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
+
+		if(x<=0) return 0;
 
 		for(int i=0; i<=x;i++)
 		{
